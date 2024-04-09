56ee92ab3a7239aa3b9d8533e462b85cf6926eb3
Denver <audgkwns44@gmail.com>
2024-04-09 11:15:44 +0900

    [Create]Pagination API for ingredients

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
+
+import csv
 
 app = FastAPI()
 
@@ -9,6 +10,24 @@ app = FastAPI()
 def read_root():
     return {"Status": "200 OK", "Purpose": "OS19", "Author": "Chanwoo"}
 
-@app.get("/ingredients/:page")
-def get_ingredients():
-    return False
+@app.get("/ingredients/{page_number}")
+def get_ingredients(page_number: int):
+    ingredients = []
+
+    ## Mocked up Ingredients Database
+    # TODO: Need to migrate to real database
+    filename = "ingredients.csv"
+
+    with open(filename, "r") as file:
+        # FIXME: Below code is using n+1 antipattern
+        # When you migrate this to the real databse,
+        # fix it to the method that not using the antipattern
+        reader = csv.reader(file)
+        for row in reader:
+            for item in row:
+                ingredients.append(item)
+        file.close()
+
+    slicedData = ingredients[page_number: page_number+10]
+
+    return {"ingredients": slicedData}
