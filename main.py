396c31a9543abb7b47abb936f07dd95533eb10c6
Denver <audgkwns44@gmail.com>
2024-04-09 10:43:13 +0900

    [Update]Basic server form

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Status": "200 OK", "Purpose": "OS19", "Author": "Chanwoo"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/ingredients/:page")
+def get_ingredients():
+    return False
