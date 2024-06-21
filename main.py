fb90fd7cd57fee64caa5f13f83213f7a9c333f3a
Denver(hajun) <denver@hajuns-MacBook-Pro(M3).local>
2024-06-21 11:33:35 +0900

    [Delete]Ingredients Qeury

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 import uvicorn
 
-import csv
-
 app = FastAPI()
 
 import firebase_admin
@@ -34,51 +32,10 @@ app.add_middleware(
 def read_root():
     return {"Status": "200 OK", "Purpose": "OS19", "Author": "Chanwoo"}
 
-@app.get("/ingredients")
-def get_ingredients(page: Optional[int] = None, query: Optional[str] = None, term: Optional[int] = 10):
-    ingredients = []
+@app.post("/getrecipe")
+async def getRecipeByGPT():
 
-    ## Mocked up Ingredients Database
-    # TODO: Need to migrate to real database
-    filename = "ingredients.csv"
-    with open(filename, "r") as file:
-        # FIXME: Below code is using n+1 antipattern
-        # When migrating this to the real databse,
-        # fix it to the method that not using the antipattern
-        reader = csv.reader(file)
-        for row in reader:
-            for item in row:
-                ingredients.append(item)
-        file.close()
-
-    result = []
-    if query is not None:
-        for ingredient in ingredients:
-            if query in ingredient:
-                result.append(ingredient)
-    else:
-        result = ingredients[page * term: page * term + term]
-
-    if len(result) == 0:
-        raise HTTPException(status_code=404, detail="Ingredients Not Found")
-    else:
-        # return {
-        #     "status": "200 OK",
-        #     "result": result
-        # }
-        return {"result": result}
-    
-# @app.post("/")
-
-# TODO: Create new ingredients
-# Recommend to make it after connect to the real databse
-# Cuz, creating to mockup DB(the csv file) is useless
-
-# TODO: Update an ingredient
-
-# TODO: Delete an ingredient
-
-# @app.get("/ingredients")
+    return True
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))  # Default to 8000 if PORT not set
